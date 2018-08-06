6e42524ab3f306450b73834bc1e459c7dc5f235f
Maxim Nechaev <4max.nechaev@gmail.com>
2018-08-06 15:22:53 +0000

    Tryout to write a simple node.js blog templare

--- app.js
+++ app.js
@@ -1,42 +1,47 @@
-// require and instantiate express
-const app = require('express')()
+/*jshint esversion: 6 */ //this is a fix for linter
 
-// fake posts to simulate a database
+// app.js
+
+//require and instantinate express
+const app = require('express')() //automatically looks inside the views/ folder for template files
+
+//fake posts to simulate a database
 const posts = [
   {
     id: 1,
     author: 'John',
     title: 'Templating with EJS',
-    body: 'Blog post number 1'
+    body: 'Blog post No 1'
   },
   {
     id: 2,
     author: 'Drake',
     title: 'Express: Starting from the Bottom',
-    body: 'Blog post number 2'
+    body: 'Blog post No 2'
   },
   {
     id: 3,
     author: 'Emma',
-    title: 'Streams',
-    body: 'Blog post number 3'
+    title: 'Stream',
+    body: 'Blog post No 3'
   },
   {
     id: 4,
     author: 'Cody',
     title: 'Events',
-    body: 'Blog post number 4'
-  }
-]
+    body: 'Blog post No 4'
+  },
+];
 
-// set the view engine to ejs
+//set the view engine to ejs
 app.set('view engine', 'ejs')
 
-// blog home page
+//blog home page
 app.get('/', (req, res) => {
-  // render `home.ejs` with the list of posts
-  res.render('home', { posts: posts })
-})
+  //find the post in the `posts` array
+  const post = posts.filter((post) => {
+    return post.id == req.params.id;
+  })[0]
 
 // blog post
 app.get('/post/:id', (req, res) => {
@@ -46,13 +51,14 @@ app.get('/post/:id', (req, res) => {
   })[0]
 
   // render the `post.ejs` template with the post content
-  res.render('post', {
+  res.render('post', { //res.render() method is used to render the view we pass it and send the HTML to the client
     author: post.author,
     title: post.title,
     body: post.body
   })
 })
 
+})
 app.listen(8080)
 
 console.log('listening on port 8080')
